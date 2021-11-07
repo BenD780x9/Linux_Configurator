430247e17fefbb1e6d9b5e955d84949ab21b07b4
Ben_D <39602072+BenD780x9@users.noreply.github.com>
2021-11-07 10:08:41 +0000

    install python & pip by OS type.

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-pip3 install -r requirements.txt 
-python3 configurator.py
+
+OS=$(uname -n)
+
+if [  $OS == "fedora" ] ; then
+    sudo dnf install -y python python-pip python-pip-wheel
+    pip3 install -r requirements.txt
+    python3 configurator.py
+fi
+
+if [  $OS == "ubuntu" ] ; then
+    eho "Add ubuntu install script"
+    pip3 install -r requirements.txt
+    python3 configurator.py
+fi
+
