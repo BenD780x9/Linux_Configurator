c55d1d5606cf1e2a9a42ee863eb9b150fbbafd0c
Ben_D <39602072+BenD780x9@users.noreply.github.com>
2021-11-07 11:13:52 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 OS=$(uname -n)
 
 if [  $OS == "fedora" ] ; then
+    sudo dnf -y update
     sudo dnf install -y python python-pip python-pip-wheel
     pip3 install -r requirements.txt
     python3 configurator.py
