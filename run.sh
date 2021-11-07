00665677b965049bb315ee0e0a4bec32f04c1199
l0x4c <89319163+l0x4c@users.noreply.github.com>
2021-11-07 14:20:10 +0200

    Put py & pip outside as they should run anyway

--- run.sh
+++ run.sh
@@ -5,13 +5,11 @@ OS=$(uname -n)
 if [  $OS == "fedora" ] ; then
     sudo dnf -y update
     sudo dnf install -y python python-pip python-pip-wheel
-    pip3 install -r requirements.txt
-    python3 configurator.py
 fi
 
 if [  $OS == "ubuntu" ] ; then
     echo "Add ubuntu install script"
-    pip3 install -r requirements.txt
-    python3 configurator.py
 fi
 
+pip3 install -r requirements.txt
+python3 configurator.py
