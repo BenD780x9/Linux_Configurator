4a61e3e5dd620eca6e5fdb56792ae6949deaecf1
l0x4c <l0x4c@users.noreply.github.com>
2021-11-09 12:47:46 +0200

    Add dnf to helper.py

--- helper.py
+++ helper.py
@@ -6,3 +6,34 @@ def is_sudo():
 
 def run_cmd(cmd):
     sp.call(cmd, stdout=sp.DEVNULL, stderr=sp.DEVNULL, shell=True)
+
+def set_hostname(hostname):
+    run_cmd(f"hostnamectl set-hostname {hostname}")
+
+class dnf:
+    def upgrade():
+        run_cmd("dnf -y upgrade --refresh")
+
+    def check():
+        run_cmd("dnf check")
+
+    def install(package, *args):
+        cmd = f"dnf install -y {package}"
+        for arg in args:
+            cmd += " " + arg
+        run_cmd(cmd)
+    
+    def group(operation, group, *args):
+        cmd = f"dnf group {operation} {group}"
+        for arg in args:
+            cmd += " " + arg
+        run_cmd(cmd)
+
+    def group_update(group, *args):
+        cmd = f"dnf groupupdate {group}"
+        for arg in args:
+            cmd += " " + arg
+        run_cmd(cmd)
+    
+    def config_manager(operation, value):
+        run_cmd(f"dnf config-manager {operation} {value}")
