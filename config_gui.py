032a14ff768c3e4d69004a40dac6823f22ff6e0c
Moshe Nahmias <moshegrey@gmail.com>
2021-11-28 21:21:06 +0200

    WIP

--- config_gui.py
+++ config_gui.py
@@ -4,57 +4,62 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QProcess
 from helper import *
 
+
 class MainWindow(QWidget):
     def __init__(self, facts):
         super().__init__()
         self.facts = facts
-        self.dic = {'cb_drivers':False, 'cb_gpu':False, 'cb_dropbox':False, 'cb_nextcloud':False, 'cb_google':False, 'cb_skype':False, 'cb_zoom':False, 'cb_chrome':False, 'cb_chromium':False}
+        self.dic = {'cb_drivers': False, 'cb_gpu': False, 'cb_dropbox': False, 'cb_nextcloud': False,
+                    'cb_google': False, 'cb_skype': False, 'cb_zoom': False, 'cb_chrome': False,
+                    'cb_chromium': False}
+        self.label = QtWidgets.QLabel(f"You are running a {self.facts.PC} PC \nYour package manager is "
+                                      f"{self.facts.package_manager.__class__.__name__} with {self.facts.GPU} GPU.\n\n"
+                                      f"Choose what to install:")
+        self.cb_drivers = QCheckBox("Install System configs and Drivers   (Recommended)", self)
+        self.cb_gpu = QCheckBox(f"Install {self.facts.GPU} drivers", self)
+        self.cb_dropbox = QCheckBox('Install Dropbox')
+        self.cb_nextcloud = QCheckBox('Install NextCloud')
+        self.cb_google = QCheckBox('Install Google Cloud')
+        self.cb_skype = QCheckBox('Install Skype')
+        self.cb_zoom = QCheckBox('Install Zoom')
+        self.cb_chrome = QCheckBox('Install Chrome')
+        self.cb_chromium = QCheckBox('Install Chromium')
+        self.btn = QPushButton("Start Install", self)
         self.initUI()
 
     def initUI(self):
         vbox = QVBoxLayout()
         
-        self.label = QtWidgets.QLabel(f"You are runnig a {self.facts.PC} PC \nYour system is {self.facts.OS} with {self.facts.GPU} GPU.\n\nChoose what to install:")
         vbox.addWidget(self.label)
         
-        self.cb_drivers = QCheckBox("Install System configs and Drivers   (Recommended)", self)
         vbox.addWidget(self.cb_drivers)
         self.cb_drivers.setChecked(True)
         self.cb_drivers.stateChanged.connect(self.checked)
 
-        self.cb_gpu = QCheckBox(f"Install {self.facts.GPU} drivers", self)
         vbox.addWidget(self.cb_gpu)
         self.cb_gpu.stateChanged.connect(self.checked)
         
-        self.cb_dropbox = QCheckBox('Install Dropbox')
         vbox.addWidget(self.cb_dropbox)
         self.cb_dropbox.stateChanged.connect(self.checked)
         
-        self.cb_nextcloud = QCheckBox('Install NextCloud')
         vbox.addWidget(self.cb_nextcloud)
         self.cb_nextcloud.stateChanged.connect(self.checked)
 
-        self.cb_google = QCheckBox('Install Google Cloud')
         vbox.addWidget(self.cb_google)
         self.cb_google.stateChanged.connect(self.checked)
         
-        self.cb_skype = QCheckBox('Install Skype')
         vbox.addWidget(self.cb_skype)
         self.cb_skype.stateChanged.connect(self.checked)
         
-        self.cb_zoom = QCheckBox('Install Zoom')
         vbox.addWidget(self.cb_zoom)
         self.cb_zoom.stateChanged.connect(self.checked)
         
-        self.cb_chrome = QCheckBox('Install Chrome')
         vbox.addWidget(self.cb_chrome)
         self.cb_chrome.stateChanged.connect(self.checked)
         
-        self.cb_chromium = QCheckBox('Install Chromium')
         vbox.addWidget(self.cb_chromium)
         self.cb_chromium.stateChanged.connect(self.checked)
         
-        self.btn = QPushButton("Start Install", self)
         vbox.addWidget(self.btn)
         self.btn.clicked.connect(self.start_installation)
 
@@ -68,45 +73,54 @@ class MainWindow(QWidget):
         self.show()
 
     def checked(self): 
-        """ If checked it change the value in the dictionery. 
+        """ If checked it change the value in the dictionary.
             In the second window the installer checks what it need to (install True = install / False = NOT install) """
         
         if self.cb_drivers.checkState():
             self.dic['cb_drivers'] = True
-            #print("DEBUG:Drivers")
+        else:
+            self.dic['cb_drivers'] = False
 
         if self.cb_gpu.checkState():
             self.dic['cb_gpu'] = True
-            #print("DEBUG:GPU drivers")
+        else:
+            self.dic['cb_gpu'] = False
 
         if self.cb_dropbox.checkState():
             self.dic['cb_dropbox'] = True
-            #print("DEBUG:dropbox")
+        else:
+            self.dic['cb_dropbox'] = False
 
         if self.cb_nextcloud.checkState():
             self.dic['cb_nextcloud'] = True
-            #print("DEBUG:nextcloud")
+        else:
+            self.dic['cb_nextcloud'] = False
 
         if self.cb_google.checkState():
             self.dic['cb_google'] = True
-            #print("DEBUG:google")
+        else:
+            self.dic['cb_google'] = False
 
         if self.cb_zoom.checkState():
             self.dic['cb_zoom'] = True
-            #print("DEBUG:zoom")
+        else:
+            self.dic['cb_zoom'] = False
 
         if self.cb_skype.checkState():
             self.dic['cb_skype'] = True
-            #print("DEBUG:skype")
+        else:
+            self.dic['cb_skype'] = False
 
         if self.cb_chrome.checkState():
             self.dic['cb_chrome'] = True
-            #print("DEBUG:chrome")
+        else:
+            self.dic['cb_chrome'] = False
 
         if self.cb_chromium.checkState():
             self.dic['cb_chromium'] = True
-            #print("DEBUG:chromium")
-    
+        else:
+            self.dic['cb_chromium'] = False
+
     def start_installation(self):
         self.win_install = WindowInstall(self.dic)
         self.win_install.show()
@@ -126,7 +140,6 @@ class WindowInstall(QMainWindow):
         self.progress.setRange(0, 100)
 
         l = QVBoxLayout()
-        #l.addWidget(self.btn)
         l.addWidget(self.progress)
         l.addWidget(self.text)
 
@@ -139,14 +152,12 @@ class WindowInstall(QMainWindow):
         self.text.appendPlainText(s)
 
     def start_process(self):
-        #if self.p is None:  # No process running.
         self.message("Executing process")
         self.p = QProcess()  # Keep a reference to the QProcess (e.g. on self) while it's running.
         self.p.readyReadStandardOutput.connect(self.handle_stdout)
         self.p.readyReadStandardError.connect(self.handle_stderr)
         self.p.stateChanged.connect(self.handle_state)
         self.p.finished.connect(self.process_finished)  # Clean up once complete.
-        #self.p.start(install_func)
         self.p.start("python3", ['dummy_script.py'])
 
     def handle_stderr(self):
--- configurator.py
+++ configurator.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 
-import os
-import subprocess
 import sys
-import time
-from pathlib import Path
-from functools import partial
-from helper import *
-from config_installer import *
+
 from config_gui import *
+from logic.facts import Facts
 
 
 def main():
@@ -17,45 +12,10 @@ def main():
         print("This script must be run as root") # replace with a QMessageBox not DEBUG
         sys.exit()
 
-    collect_facts(facts)
+    facts.collect_facts()
     app = QApplication(sys.argv)
     win = MainWindow(facts)
     sys.exit(app.exec_())
-
-def collect_facts(facts):
-    facts.HOME = str(Path.home())
-
-    # Determine the Distro and Desktop Enviroment #
-    distro = os.popen("cat /etc/*release").read()
-    if "fedora" in distro:
-        facts.OS = "Fedora"
-        if "KDE" in distro:
-            facts.DE = "KDE"
-        else:
-            facts.DE = "GNOME"
-    elif "ubuntu" in distro:
-        facts.OS = "Ubuntu"
-        if "KDE" in distro:
-            facts.DE = "KDE"
-        else:
-            facts.DE = "GNOME"
-
-    # Determine which GPU is in the PC #
-    gpu = os.popen("lspci | grep VGA").read().lower()
-    if "intel" in gpu:
-        facts.GPU = "Intel"
-    elif "amd" in gpu:
-        facts.GPU = "AMD"
-    elif "nvidia" in gpu:
-        facts.GPU = "Nvidia"
-    else:
-        facts.GPU = "Unknown"
-
-    # Determine if the computer is PC or a Laptop #
-    if not os.path.exists("/proc/acpi/button/lid"):
-        facts.PC = "Desktop"
-    else:
-        facts.PC = "Laptop"
 
 
 if __name__ == "__main__":
