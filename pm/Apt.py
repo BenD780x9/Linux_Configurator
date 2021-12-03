c04e51d47d3c3de7a60eaa2d551481e958bf79fc
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2021-12-03 02:31:41 +0200

    added req.txt and changed pm

--- pm/Apt.py
+++ pm/Apt.py
@@ -1,2 +1,23 @@
+from typing import List
+from .packages.Models.Package import Package
+from .utils import run_cmd
+import json
+
+
 class Apt:
-    pass
+    def __init__(self) -> None:
+        pass
+    
+    
+    
+    def load_packages() -> List[Package]:
+        with open('./packages/apt.json') as f:
+            data = json.load(f)
+        return [Package(**p) for p in data]
+        
+    def install(pkg: Package):
+        for command in pkg.commands:
+            run_cmd(command)
+
+
+
